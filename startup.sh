613915d35ed8cfd7a9e9553bddc6ab87c2c58dc6
Sriram Suresh <sriram.suresh449@gmail.com>
2025-03-22 19:47:36 -0500

    added instructions on how to read and write to the created proc file

--- startup.sh
+++ startup.sh
@@ -3,3 +3,8 @@ sudo insmod ldd.ko
 lsmod | grep ldd
 ls /proc | grep pyjama
 sudo dmesg
+
+# here is how to read and write to the proc file
+# echo "hi pyjama driver" > /proc/pyjama_driver # write
+# cat /proc/pyjama_driver # read
+
